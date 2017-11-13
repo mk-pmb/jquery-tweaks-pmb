775ec87aef351bc54a2c37bab39c875d9445ae0d
mk-pmb <mk-pmb@users.noreply.github.com>
2017-11-13 14:34:13 +0100

    v0.1.0

---
+++ README.md
@@ -0,0 +1,44 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+jquery-tweaks-pmb
+=================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Some tweaks for jQuery.
+<!--/#echo -->
+
+* AJAX tweaks
+  * Override the server response's MIME type for `dataType`s
+    `text` and `json` to evade browsers' XML parsing attempts
+    in case of unfortunate MIME header.
+
+
+
+Usage
+-----
+
+```html
+<script src="node_modules/jquery-tweaks-pmb/t.js"></script>
+```
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* Needs more/better tests and docs.
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ t.js
@@ -0,0 +1,35 @@
+﻿/*jslint indent: 2, maxlen: 80, browser: true */
+/* -*- tab-width: 2 -*- */
+/*global define: true */
+(function () {
+  'use strict';
+  var EX = {}, rqr = ((typeof require === 'function') && require),
+    jq = (rqr ? rqr('jquery') : window.jQuery);
+
+  function noop() { return; }
+
+  EX.dataType2mime = {
+    text: 'text/plain',
+    json: 'text/json',
+  };
+
+  EX.tweakAjaxOpts = function (opt, origOpt, jqXHR) {
+    noop(origOpt);
+    var mimeOvr = opt.overrideResponseMimeType;
+    if (mimeOvr === undefined) { mimeOvr = EX.dataType2mime[opt.dataType]; }
+    if (mimeOvr !== undefined) { jqXHR.overrideMimeType(mimeOvr); }
+  };
+  jq.ajaxPrefilter(EX.tweakAjaxOpts);
+
+
+
+
+
+
+  (function unifiedExport(e) {
+    var d = ((typeof define === 'function') && define),
+      m = ((typeof module === 'object') && module);
+    if (d && d.amd) { d(function () { return e; }); }
+    if (m && m.exports) { m.exports = e; }
+  }(EX));
+}());
